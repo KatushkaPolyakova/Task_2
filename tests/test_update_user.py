973b4471ad0874b06838822b42edaed88e0318f6
Katushka <litvtnovae@mail.ru>
2026-07-20 16:05:50 +0700

    change test update user

--- tests/test_update_user.py
+++ tests/test_update_user.py
@@ -1,49 +1,79 @@
-import pytest
 import requests
 import allure
 from url import URL, UPDATE_USER
+from helpers import generate_user
+from data import UNAUTHORIZED
 
 
 class TestUpdateUser: 
 
-    @pytest.mark.parametrize('field, value', 
-                             [
-                                 ('email', 'new_email_new_@mail.ru'),
-                                 ('password', 'new_password'),
-                                 ('name', 'new_name'),
-                             ] )
-    @allure.title("Изменение данных авторизованного пользователя")
-    def test_update_after_auth_success(self, create_user, field, value):
-        token = create_user['token']
-        payload = {field:value}
+    @allure.title("Изменение email авторизованного пользователя")
+    def test_update_email_after_auth_success(self, create_user):
+        token = create_user["token"]
+        new_email = generate_user()["email"]
 
-        with allure.step("Отправить запрос на измнение данных"):
-            response = requests.patch(URL+UPDATE_USER, headers={'Authorization': token}, json=payload)
+        with allure.step("Отправить запрос на изменение email"):
+            response = requests.patch(URL + UPDATE_USER,headers={"Authorization": token},json={"email": new_email})
 
         with allure.step("Проверить ответ сервера"):
             assert response.status_code == 200
-            assert response.json()['success'] is True
-            if field == 'email':
-                assert response.json()['user']['email'] ==  value
-            elif field == 'name':
-                assert response.json()['user']['name'] == value
+            assert response.json()["success"] is True
+            assert response.json()["user"]["email"] == new_email
 
-
-    @pytest.mark.parametrize('field, value', 
-                             [
-                                 ('email', 'new_email@mail.ru'),
-                                 ('password', 'new_password'),
-                                 ('name', 'new_name'),
-                             ] )
-    @allure.title("Изменение данных пользователя без авторизации")
-    def test_update_user_without_auth_error(self, field, value):
-        payload = {field:value}
         
-        with allure.step("Отправить запрос на измнение данных без авторизации"):
-            response = requests.patch(URL+UPDATE_USER, json=payload)
+    @allure.title("Изменение имени авторизованного пользователя")
+    def test_update_name_after_auth_success(self, create_user):
+        token = create_user["token"]
+
+        with allure.step("Отправить запрос на изменение имени"):
+            response = requests.patch(URL + UPDATE_USER,headers={"Authorization": token},json={"name": "new_name"})
+
+        with allure.step("Проверить ответ сервера"):
+            assert response.status_code == 200
+            assert response.json()["success"] is True
+            assert response.json()["user"]["name"] == "new_name"
+
+
+    @allure.title("Изменение пароля авторизованного пользователя")
+    def test_update_password_after_auth_success(self, create_user):
+        token = create_user["token"]
+
+        with allure.step("Отправить запрос на изменение пароля"):
+            response = requests.patch(URL + UPDATE_USER,headers={"Authorization": token},json={"password": "new_password"})
+
+        with allure.step("Проверить ответ сервера"):
+            assert response.status_code == 200
+            assert response.json()["success"] is True
+
+    @allure.title("Изменение email без авторизации")
+    def test_update_email_without_auth_error(self):
+
+        with allure.step("Отправить запрос на изменение email"):
+            response = requests.patch(URL + UPDATE_USER,json={"email": generate_user()["email"]})
 
         with allure.step("Проверить ответ сервера"):
             assert response.status_code == 401
-            assert response.json()['success'] is False
-            assert response.json()['message'] == 'You should be authorised'
+            assert response.json()["success"] is False
+            assert response.json()["message"] == UNAUTHORIZED
 
+    @allure.title("Изменение имени без авторизации")
+    def test_update_name_without_auth_error(self):
+
+        with allure.step("Отправить запрос на изменение имени"):
+            response = requests.patch(URL + UPDATE_USER,json={"name": "new_name"})
+
+        with allure.step("Проверить ответ сервера"):
+            assert response.status_code == 401
+            assert response.json()["success"] is False
+            assert response.json()["message"] == UNAUTHORIZED
+
+    @allure.title("Изменение пароля без авторизации")
+    def test_update_password_without_auth_error(self):
+
+        with allure.step("Отправить запрос на изменение пароля"):
+            response = requests.patch(URL + UPDATE_USER,json={"password": "new_password"})
+
+        with allure.step("Проверить ответ сервера"):
+            assert response.status_code == 401
+            assert response.json()["success"] is False
+            assert response.json()["message"] == UNAUTHORIZED
