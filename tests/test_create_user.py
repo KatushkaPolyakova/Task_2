564b76d1ad805b59bb05566cd6d3e1906c66c096
Katushka <litvtnovae@mail.ru>
2026-07-20 15:11:40 +0700

    add steps in test_create_user

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,7 +1,7 @@
 import pytest 
 import requests
 import allure 
-from url import URL, CREATE_USER
+from url import URL, CREATE_USER, UPDATE_USER
 from helpers import generate_user
 from data import USER_ALREADY_EXISTS, REQUIRED_FIELDS
 
@@ -9,23 +9,39 @@ from data import USER_ALREADY_EXISTS, REQUIRED_FIELDS
 class TestCreateUser:
 
     @allure.title("Успешное создание уникального пользователя")
-    def test_create_unique_user_success(self, create_user):
-        response = create_user['response']
+    def test_create_unique_user_success(self):
+        user = generate_user()
+        
+        with allure.step("Отправить запрос на создание пользователя"):
+            response = requests.post(URL+CREATE_USER, json=user)
+
         with allure.step("Проверить ответ сервера"):
             assert response.status_code == 200
             assert response.json()['success'] is True
 
+        with allure.step("Удалить пользователя"):
+            token = response.json()["accessToken"]
+            requests.delete(URL + UPDATE_USER,headers={"Authorization": token})
+
 
     @allure.title("Повторное создание пользователя")
-    def test_create_same_user_error(self, create_user):
+    def test_create_same_user_error(self):
+        user = generate_user()
+
+        with allure.step("Отправить запрос на создание пользователя"):
+            response = requests.post(URL+CREATE_USER, json=user)
 
         with allure.step("Отправить запрос на повторное создание пользователя"):
-            response = requests.post(URL+CREATE_USER, json=create_user['user'])
+            second_response = requests.post(URL+CREATE_USER, json=user)
         
         with allure.step("Проверить ответ сервера"):
-            assert response.status_code == 403
-            assert response.json()['message'] == USER_ALREADY_EXISTS
-            assert response.json()['success'] is False
+            assert second_response.status_code == 403
+            assert second_response.json()['message'] == USER_ALREADY_EXISTS
+            assert second_response.json()['success'] is False
+
+        with allure.step("Удалить пользователя"):
+            token = response.json()["accessToken"]
+            requests.delete(URL + UPDATE_USER,headers={"Authorization": token})
 
 
     @pytest.mark.parametrize('field',['email', 'password', 'name'])
